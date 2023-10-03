c9075169bb0fcb1d4e8072bd7172477772df5d0a
evelync araujo <evyaraujoifc@gmail.com>
2023-10-03 16:41:08 -0300

    fim-da-aula-02/09

---
+++ src/stores/genres.js
@@ -0,0 +1,20 @@
+import { reactive, computed } from 'vue'
+import { defineStore } from 'pinia'
+import api from '@/plugins/axios'
+
+export const useGenreStore = defineStore('genre', () =>{
+    const state = reactive({
+        genres: []
+    })
+
+    const genres = computed(() => state.genres)
+    const getGenreName = (id) => state.genres.find((genre) => genre.id === id.name)
+
+    const getAllGenres = async (type) => {
+        const response = await api.get(`genre/${type}/list?language=pt-BR`)
+        state.genres = responde.data.genres
+    }
+
+    return {genres, gatAllGenres, getGenreName}
+
+})
